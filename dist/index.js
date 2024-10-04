235bbd8fc6e2bb20e056c4a22533168045937ecf
Repooc <tjcooper1982@gmail.com>
2024-10-04 01:43:33 -0400

    build: update distribution (#6)
    Co-authored-by: Repooc <1479745+Repooc@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -34691,13 +34691,36 @@ const fs = __nccwpck_require__(9896);
 const axios = __nccwpck_require__(7269);
 const path = __nccwpck_require__(6928);
 
-const PATRON_CAMPAIGN_ID = process.env.PATRON_CAMPAIGN_ID;
-const PATRON_ACCESS_TOKEN = process.env.PATRON_ACCESS_TOKEN;
-const getTiers = process.env.GET_TIERS || false;
-const luaFilePath = process.env.FILE_PATH_AND_NAME || 'patrons.lua'; // you can change the file path and name here ex: './test/patrons.lua'
-
+const PATRON_CAMPAIGN_ID = core.getInput('patron_campaign_id') || process.env.PATRON_CAMPAIGN_ID;
+const PATRON_ACCESS_TOKEN = core.getInput('patron_access_token') || process.env.PATRON_ACCESS_TOKEN;
 if (!PATRON_CAMPAIGN_ID || !PATRON_ACCESS_TOKEN) {
-  throw new Error('PATRON_CAMPAIGN_ID and PATRON_ACCESS_TOKEN must be set');
+  core.setFailed('PATRON_CAMPAIGN_ID and PATRON_ACCESS_TOKEN must be set');
+}
+
+const outputFormat = core.getInput('output_format') || process.env.OUTPUT_FORMAT || 'lua'; // Allowed values: 'lua' or 'json'
+// Validate the output format to allow only 'json' or 'lua'
+const validFormats = ['lua', 'json'];
+if (!validFormats.includes(outputFormat)) {
+  core.setFailed(`Invalid output format. Must be one of: ${validFormats.join(', ')}`);
+}
+
+// const getTiers = core.getBooleanInput('get_tiers') || (process.env.GET_TIERS ? process.env.GET_TIERS.toLowerCase() === 'true' : false);
+const isGetTiers = core.getInput('get_tiers') || (process.env.GET_TIERS || 'false').toLowerCase();
+// const getTiers = isGetTiers === 'true';
+const getTiers = false; // Disabled for now
+
+const filePath = core.getInput('file_path') || process.env.FILE_PATH || './';
+let fileName = core.getInput('file_name') || process.env.FILE_NAME || 'patrons.lua';
+
+// Ensure the file extension matches the output format
+if (outputFormat === 'json' && !fileName.endsWith('.json')) {
+  const oldFileName = fileName;
+  fileName = fileName.replace(/\.[^/.]+$/, '') + '.json'; // Change to .json
+  core.notice(`File name changed from '${oldFileName}' to '${fileName}' to match the JSON output format.`);
+} else if (outputFormat === 'lua' && !fileName.endsWith('.lua')) {
+  const oldFileName = fileName;
+  fileName = fileName.replace(/\.[^/.]+$/, '') + '.lua'; // Change to .lua
+  core.notice(`File name changed from '${oldFileName}' to '${fileName}' to match the Lua output format.`);
 }
 
 const headers = {
@@ -34742,77 +34765,72 @@ async function fetchPatrons(
 ) {
   const patrons = [];
 
-  try {
-    while (pageUrl) {
-      const response = await axios.get(pageUrl, { headers: headers });
-      patrons.push(...response.data.data);
+  while (pageUrl) {
+    const response = await axios.get(pageUrl, { headers: headers });
+    patrons.push(...response.data.data);
+    pageUrl = response.data.links?.next;
 
-      pageUrl = response.data.links?.next;
-
-      core.info(`Fetching ${response.data.data.length} patrons`);
-    }
-    core.info('Total Patrons Fetched:', patrons.length);
-
-    return patrons;
-  } catch (error) {
-    if (error.response) {
-      core.debug(inspect('Error Response:', error.response.status, error.response.data));
-      core.setFailed(error.response.status, error.response.data);
-    } else {
-      core.debug(inspect('Error Response:', error.response.status, error.response.data));
-      core.setFailed(error.message);
-    }
+    core.info(`Fetching ${response.data.data.length} patrons`);
   }
+
+  core.info('Total Patrons Fetched:', patrons.length);
+  return patrons;
 }
 
 function saveToFile(patrons) {
   const topPatron = {};
-
-  // Build the Lua table
-  let luaData = 'local Patrons = {\n';
-
   patrons.forEach((patron) => {
     if (!topPatron.full_name || patron.attributes.lifetime_support_cents > topPatron.lifetime_support_cents) {
       topPatron.full_name = patron.attributes.full_name;
       topPatron.lifetime_support_cents = patron.attributes.lifetime_support_cents;
     }
   });
+
   core.info(`Top patron: ${topPatron.full_name} with lifetime_support_cents: ${topPatron.lifetime_support_cents}`);
 
-  patrons.forEach((patron) => {
-    luaData += `  { name = "${patron.attributes.full_name}", isActive = ${patron.attributes.patron_status === 'active_patron' ? 'true' : 'false'}, isTopPatron = ${patron.attributes.full_name === topPatron.full_name ? 'true' : 'false'} },\n`;
-  });
-  luaData += '}\n';
+  const fullFilePath = path.join(filePath, fileName);
 
-  try {
-    // Check if the file exists
-    if (fs.existsSync(luaFilePath)) {
-      const fileContent = fs.readFileSync(luaFilePath, 'utf8');
-      const membersTableRegex = /local Patrons = {(?:\n*\s*{[^}]*},?)*\n*}/; // Regex to find the Patrons table
+  if (outputFormat === 'json') {
+    const jsonData = patrons.map((patron) => ({
+      name: patron.attributes.full_name,
+      isActive: patron.attributes.patron_status === 'active_patron',
+      isTopPatron: patron.attributes.full_name === topPatron.full_name,
+    }));
+
+    if (!fs.existsSync(filePath)) {
+      core.notice(`${filePath} does not exist. Creating ${filePath} path for you now.`);
+      fs.mkdirSync(filePath, { recursive: true });
+      core.notice(`Successfully created the ${filePath} path.`);
+    }
+    fs.writeFileSync(fullFilePath, JSON.stringify(jsonData, null, 2), 'utf8');
+    core.notice(`Patreon data saved as JSON to ${fullFilePath}`);
+  } else {
+    let luaData = 'local Patrons = {\n';
+
+    patrons.forEach((patron) => {
+      luaData += `  { name = "${patron.attributes.full_name}", isActive = ${patron.attributes.patron_status === 'active_patron' ? 'true' : 'false'}, isTopPatron = ${patron.attributes.full_name === topPatron.full_name ? 'true' : 'false'} },\n`;
+    });
+    luaData += '}\n';
+
+    if (!fs.existsSync(filePath)) {
+      fs.mkdirSync(filePath, { recursive: true });
+    }
+    if (fs.existsSync(fullFilePath)) {
+      const fileContent = fs.readFileSync(fullFilePath, 'utf8');
+      const membersTableRegex = /local Patrons = {(?:\n*\s*{[^}]*},?)*\n*}/;
 
       if (membersTableRegex.test(fileContent)) {
         // Replace the existing table
         const updatedContent = fileContent.replace(membersTableRegex, luaData.trim());
-        fs.writeFileSync(luaFilePath, updatedContent, 'utf8');
+        fs.writeFileSync(fullFilePath, updatedContent, 'utf8');
       } else {
         // Prepend the new table to the existing content
-        fs.writeFileSync(luaFilePath, luaData + '\n' + fileContent, 'utf8');
+        fs.writeFileSync(fullFilePath, luaData + '\n' + fileContent, 'utf8');
       }
     } else {
-      // Ensure the directory exists
-      const dir = path.dirname(luaFilePath);
-      if (!fs.existsSync(dir)) {
-        // Create the directory recursively
-        fs.mkdirSync(dir, { recursive: true });
-      }
-
-      // Create the file
-      fs.writeFileSync(luaFilePath, luaData, 'utf8');
+      fs.writeFileSync(fullFilePath, luaData, 'utf8');
     }
-    core.notice(`Patreon data saved to ${luaFilePath}`);
-  } catch (error) {
-    core.debug(inspect(error));
-    core.setFailed(error.message);
+    core.notice(`Patreon data saved to ${fullFilePath}`);
   }
 }
 
